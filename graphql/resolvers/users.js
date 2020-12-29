e6297e7870c445e9a388505b2b8d0c4adca144a3
Aashish Prasad <52325555+aashishwastaken@users.noreply.github.com>
2020-12-29 18:36:41 +0530

    Update users.js

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -25,7 +25,7 @@ async function getAccessTokenFromCode(code) {
         data: {
             client_id: process.env.GOOGLE_CLIENT_ID,
             client_secret: process.env.GOOGLE_CLIENT_SECRET,
-            redirect_uri: 'https://merng-social-network.netlify.app/authenticate/google',
+            redirect_uri: 'https://merng-social-network-client.herokuapp.com/authenticate/google',
             grant_type: 'authorization_code',
             code,
         },
